fdd3ea1664b923576c56482a5d70f8759eafa43a
Zusoy <gregoire.drapeau79@gmail.com>
2022-08-05 14:32:21 +0200

    Added messaging specs

---
+++ apps/api/tests/spec/Application/Messaging/Map/InMemoryHandlerMapSpec.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace spec\Application\Messaging\Map;
+
+use Application\Messaging\Exception\HandlerNotFoundException;
+use Application\Messaging\Map\HandlerMap;
+use Assert\AssertionFailedException;
+use Domain\Handler;
+use Domain\Message;
+use PhpSpec\ObjectBehavior;
+
+class InMemoryHandlerMapSpec extends ObjectBehavior
+{
+    public function it_is_initializable(): void
+    {
+        $this->beConstructedWith([
+            $this->createMockedHandler($this->createMockedMessage(), 'Hello'),
+            $this->createMockedHandler($this->createMockedMessage(), 'Bye')
+        ]);
+
+        $this->shouldImplement(HandlerMap::class);
+        $this->shouldNotThrow()->duringInstantiation();
+    }
+
+    public function it_throws_when_handler_not_callable(): void
+    {
+        $invalidHandler = new class implements Handler {
+            public function supports(Message $message): bool
+            {
+                return true;
+            }
+        };
+
+        $this->beConstructedWith([
+            $this->createMockedHandler($this->createMockedMessage(), 'Hello'),
+            $invalidHandler
+        ]);
+
+        $this->shouldThrow(AssertionFailedException::class)->duringInstantiation();
+    }
+
+    public function it_provides_handler_from_message(): void
+    {
+        $message1 = $this->createMockedMessage();
+        $message2 = $this->createMockedMessage();
+
+        $handler1 = $this->createMockedHandler($message1, 'Hello');
+        $handler2 = $this->createMockedHandler($message2, 'Bye');
+
+        $this->beConstructedWith([$handler1, $handler2]);
+
+        $this->getHandler($message2)->shouldBeLike($handler2);
+    }
+
+    public function it_throws_exception_when_handler_not_found(): void
+    {
+        $message1 = $this->createMockedMessage();
+        $message2 = $this->createMockedMessage();
+        $message3 = $this->createMockedMessage();
+
+        $handler1 = $this->createMockedHandler($message1, 'Hello');
+        $handler2 = $this->createMockedHandler($message2, 'Bye');
+
+        $this->beConstructedWith([$handler1, $handler2]);
+
+        $this
+            ->shouldThrow(HandlerNotFoundException::class)
+            ->during('getHandler', [$message3])
+        ;
+    }
+
+    private function createMockedMessage(): Message
+    {
+        return new class implements Message
+        {
+        };
+    }
+
+    private function createMockedHandler(Message $supportedMessage = null, mixed $invocationResult): Handler
+    {
+        return new class($supportedMessage, $invocationResult) implements Handler
+        {
+            public function __construct(private Message $supportedMessage, private mixed $invocationResult)
+            {
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            public function supports(Message $message): bool
+            {
+                return $this->supportedMessage === $message;
+            }
+
+            public function __invoke(Message $message): mixed
+            {
+                return $this->invocationResult;
+            }
+        };
+    }
+}
---
+++ apps/api/tests/spec/Application/Messaging/TransactionalMessageBusSpec.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace spec\Application\Messaging;
+
+use Application\Messaging\Map\InMemoryHandlerMap;
+use Application\Messaging\Transaction\Transaction;
+use Domain\Bus;
+use Domain\Handler;
+use Domain\Message;
+use PhpSpec\ObjectBehavior;
+
+class TransactionalMessageBusSpec extends ObjectBehavior
+{
+    public function it_is_initializable(Transaction $transaction): void
+    {
+        $handler1 = $this->createMockedHandler($this->createMockedMessage(), 'Hello World!');
+        $handler2 = $this->createMockedHandler($this->createMockedMessage(), 'Hello World!');
+
+        $this->beConstructedWith(
+            new InMemoryHandlerMap([$handler1, $handler2]),
+            $transaction
+        );
+
+        $this->shouldImplement(Bus::class);
+        $this->shouldNotThrow()->duringInstantiation();
+    }
+
+    public function it_returns_the_supported_handler_result(Transaction $transaction): void
+    {
+        $message = $this->createMockedMessage();
+        $handler1 = $this->createMockedHandler($message, 'Hello World !');
+        $handler2 = $this->createMockedHandler($message, 'Bye !');
+
+        $this->beConstructedWith(new InMemoryHandlerMap([$handler1, $handler2]), $transaction);
+
+        $this->execute($message)->shouldReturn('Hello World !');
+    }
+
+    private function createMockedMessage(): Message
+    {
+        return new class implements Message
+        {
+        };
+    }
+
+    private function createMockedHandler(Message $supportedMessage = null, mixed $invocationResult): Handler
+    {
+        return new class($supportedMessage, $invocationResult) implements Handler
+        {
+            public function __construct(private Message $supportedMessage, private mixed $invocationResult)
+            {
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            public function supports(Message $message): bool
+            {
+                return $this->supportedMessage === $message;
+            }
+
+            public function __invoke(Message $message): mixed
+            {
+                return $this->invocationResult;
+            }
+        };
+    }
+}
