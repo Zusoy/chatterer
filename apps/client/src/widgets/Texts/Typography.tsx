ee4ccadc5df7885ee080e8d3ea8e94607d7abb5e
Zusoy <gregoire.drapeau79@gmail.com>
2024-05-07 13:26:10 +0200

    Fixed react types v

--- apps/client/src/widgets/Texts/Typography.tsx
+++ apps/client/src/widgets/Texts/Typography.tsx
@@ -4,7 +4,7 @@ import { Typography as MatTypo, type TypographyProps } from '@material-tailwind/
 export type Props = TypographyProps
 
 const Typography: React.FC<Props> = ({ children, ...props }) =>
-  <MatTypo {...props} placeholder={undefined}>
+  <MatTypo {...props}>
     {children}
   </MatTypo>
 
